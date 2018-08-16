e07e408589b8cc9cb139bbfe564aa48b2af96d4e
SusiG <bhagyalakshmi.guntamadugu@gmail.com>
2018-08-16 14:23:11 +0530

    Initial setup

---
+++ PhotoEffects/CommonClass/CommonClass.swift
@@ -0,0 +1,13 @@
+//
+//  CommonClass.swift
+//  PhotoEffects
+//
+//  Created by Bhagya on 16/08/18.
+//  Copyright © 2018 Bhagya. All rights reserved.
+//
+
+import UIKit
+
+class CommonClass: NSObject {
+
+}
